1f176f415a72e6f9d20c015b2f591005b5434636
CirroBuild <cirrobuild10@gmail.com>
2023-01-10 23:29:47 -0800

    rm bin and obj files

--- Frosti/Shared/ArgumentFlags.cs
+++ Frosti/Shared/ArgumentFlags.cs
@@ -8,20 +8,23 @@ namespace Frosti.Shared;
 public class ArgumentFlags
 {
     [Option('c', "cloud", HelpText = "Please specify the cloud with -c. See docs on getting started here: {doc-link}", Default = Clouds.Azure)]
-    public string Cloud { get; set; }
+    public string Cloud { get; set; } = Clouds.Azure;
 
     [Option('n', "name", Required = true, HelpText = "Please specify the name to prefix the infrastructure with -n. See docs on getting started here: {doc-link}")]
-    public string ProjectName { get; set; }
+    public string ProjectName { get; set; } = string.Empty;
 
     [Option('e', "sku", HelpText = "Please specify the name to prefix the infrastructure with -e. Please see doc {insert doc link}", Default = Environments.Dev)]
-    public string Enviornment { get; set; }
+    public string Enviornment { get; set; } = Environments.Dev;
 
     [Option('l', "location", HelpText = "Please specify the primary region for the resources with -l. Please see doc {insert doc link}", Default = Locations.NorthAmerica)]
-    public string Location { get; set; }
+    public string Location { get; set; } = Locations.NorthAmerica;
 
     [Option('f', "framework", HelpText = "The language or framework the project is written in, i.e. dotnet, python etc. See doc here {link}")]
     public string? Framework { get; set; }
 
     [Option('s', "subscriptionId", HelpText = "The subscription Id to be used for azure...change this later")]
     public string? SubscriptionId { get; set; }
+
+    [Option('k', "skipLinking", HelpText = "Set to true if you plan to link your resources manually. See docs on getting started here: {doc-link}", Default = false)]
+    public bool SkipLinking { get; set; } = false;
 }
